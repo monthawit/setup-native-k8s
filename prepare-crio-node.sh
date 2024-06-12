b2e7afc2d08994450f769e61ebcfb1be0cb9836b
NoobieDEV <monthawit.milky@gmail.com>
2024-06-12 16:54:28 +0700

    Create prepare-crio-node.sh

---
+++ prepare-crio-node.sh
@@ -0,0 +1,88 @@
+OS=xUbuntu_22.04
+VERSION=1.30
+
+sudo swapoff -a
+sudo rm /swap.img
+sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+
+sudo apt update -y
+sudo apt -y upgrade
+
+
+
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+
+curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | apt-key add -
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | apt-key add -
+
+sudo apt update -y
+
+sudo apt install cri-o cri-o-runc -y
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable crio --now crio
+
+sudo apt install cri-tools -y 
+
+crictl version
+
+sudo apt update -y
+apt-get install open-iscsi -y
+apt-get install nfs-common -y
+
+############# net-tools ################
+
+apt install net-tools -y
+
+############### Nginx for API LB #######
+
+#sudo apt update -y
+#sudo apt install nginx -y
+
+#systemctl enable nginx 
+#systemctl start nginx
+#systemctl status nginx
+
+####################################################
+### create dir key for ubuntu older than 22.04 ####
+
+##sudo mkdir -m 755 /etc/apt/keyrings
+
+####################################################
+
+sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update -y
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+#fix version. please insert version number
+#sudo apt-get install -y kubelet=1.28.2-00 kubeadm=1.28.2-00 kubectl=1.28.2-00
+#sudo apt-mark hold kubelet=1.28.2-00 kubeadm=1.28.2-00 kubectl=1.28.2-00
