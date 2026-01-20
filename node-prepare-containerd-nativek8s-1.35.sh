07bc1236c072904dee3677491cae26528600cdda
NoobieDEV <monthawit.milky@gmail.com>
2026-01-20 14:24:27 +0700

    Create node-prepare-containerd-nativek8s-1.35.sh

---
+++ node-prepare-containerd-nativek8s-1.35.sh
@@ -0,0 +1,106 @@
+### Disable SWAP ###
+
+sudo swapoff -a
+sudo rm /swap.img
+sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
+
+### Config OS Network Mofule ###
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+
+
+sudo apt update -y
+sudo apt -y upgrade
+
+##################### Containerd ################################
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update -y
+
+sudo apt install containerd.io -y
+
+
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+# config containerd systemdCgroup 
+
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+
+sudo systemctl restart containerd 
+sudo systemctl enable containerd
+
+
+
+
+### install storage Client ####
+sudo apt update -y
+apt-get install open-iscsi -y
+apt-get install nfs-common -y
+
+############# net-tools ################
+
+apt install net-tools -y
+
+####################################################
+### create dir key for ubuntu older than 22.04 ####
+
+sudo mkdir -m 755 /etc/apt/keyrings
+
+#################### install kubeadm kubelet ########################## 
+
+sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+
+# If the directory `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
+# sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.35/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.35/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo systemctl enable --now kubelet
+
+
+# install bash-completion
+sudo apt-get install bash-completion -y
+
+# Add the completion script to your .bashrc file
+echo 'source /etc/bash_completion' >>~/.bashrc
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+
+# Apply changes
+source ~/.bashrc
